2c4f0c2acd647274b5ab6c8dc8d4800bf321f792
irene macri <irene.macri97@gmail.com>
2019-10-31 17:33:19 +0100

    Added Test Cases

---
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsAdvancedTest.java
@@ -0,0 +1,19 @@
+package it.unical.demacs.ingsw2019.PracticeTestB;
+
+import org.junit.Test;
+
+public class MuseumsAdvancedTest {
+
+	@Test( expected = RuntimeException.class)
+	public void noOneArtwork(){
+		LouvreMuseum louvre = null; 
+		
+		louvre.getAverageRate(); 
+	}
+	@Test (timeout = 2)
+	public void twoSecondsRandom() {
+		
+		RandomMuseum random= new RandomMuseum(1000); 
+		
+	}
+}
--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
@@ -1,5 +1,33 @@
 package it.unical.demacs.ingsw2019.PracticeTestB;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class MuseumsTest {
 
+
+	@Test
+	public void LouvreMediumHigherBritishMedium() {
+		LouvreMuseum louvre = new LouvreMuseum(); 
+		BritishMuseum british = new BritishMuseum(); 
+		
+		//System.out.println(louvre.getAverageRate() + " " + british.getAverageRate());
+		assertTrue(louvre.getAverageRate()>british.getAverageRate()); 
+	}
+	@Test
+	public void LouvreMediumHigherRandomMedium() {
+		LouvreMuseum louvre = new LouvreMuseum(); 
+		RandomMuseum random = new RandomMuseum(100); 
+		
+		//System.out.println(louvre.getAverageRate() + " " + random.getAverageRate());
+		assertTrue(louvre.getAverageRate()>random.getAverageRate());
+	}
+	@Test
+	public void differenceWorks() {
+		RandomMuseum random = new RandomMuseum(1000); 
+		RandomMuseum random_Second = new RandomMuseum(1000); 
+		System.out.println(Math.abs(random.getAverageRate()-random_Second.getAverageRate()));
+		assertTrue(Math.abs(random.getAverageRate()-random_Second.getAverageRate())<1.5); 
+	}
 }
