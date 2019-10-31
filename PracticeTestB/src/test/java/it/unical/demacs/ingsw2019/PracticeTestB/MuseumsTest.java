4333a1768203cf975ee46ce6899d08fb8a822410
micmu <micmu@160.97.136.73>
2019-10-31 16:53:04 +0100

    test

--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
@@ -1,5 +1,16 @@
 package it.unical.demacs.ingsw2019.PracticeTestB;
 
-public class MuseumsTest {
+import org.junit.Test;
 
+public class MuseumsTest {
+	
+	public static LouvreMuseum lm;
+	public static BritishMuseum bm;
+	
+	@Test
+	public void medLB() {
+		
+			
+			
+	}
 }
