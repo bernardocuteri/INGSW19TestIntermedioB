0ca260333ba3808d90ed03a451efad12c6a03452
luca laboccetta <luca@luca-ThinkPad-T460s>
2019-10-31 17:32:52 +0100

    Rinominata classe MuseumAdvancedTest to MuseumExtraTest.

---
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumExtraTest.java
@@ -0,0 +1,19 @@
+package it.unical.demacs.ingsw2019.PracticeTestB;
+
+import org.junit.Test;
+
+
+
+public class MuseumExtraTest {
+	@Test(expected=RuntimeException.class)
+	public void museoSenzaOpere() {
+		RandomMuseum r=new RandomMuseum(0);
+		double n=r.getAverageRate();
+	}
+	@Test(timeout=2000)
+	public void verificaConTimeout() {
+		RandomMuseum r=new RandomMuseum(1000);
+	}
+	
+
+}
---
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTestSuite.java
@@ -0,0 +1,12 @@
+package it.unical.demacs.ingsw2019.PracticeTestB;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value=Suite.class)
+@SuiteClasses(value= {MuseumsTest.class,MuseumExtraTest.class})
+
+public class MuseumsTestSuite {
+	
+}
