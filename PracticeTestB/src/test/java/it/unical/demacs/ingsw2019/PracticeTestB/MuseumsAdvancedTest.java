df610d600e24d9998a5265f80d6b2d5f00d812a3
Anna <Maria@160.97.137.154>
2019-10-31 16:29:04 +0100

    commit first branch

---
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsAdvancedTest.java
@@ -0,0 +1,21 @@
+package it.unical.demacs.ingsw2019.PracticeTestB;
+
+import org.junit.Test;
+
+public class MuseumsAdvancedTest {
+
+	//un metodo che verifica che getAverageRate() 
+	//sollevi un’eccezione su un museo senza opere
+	@Test(expected = RuntimeException.class)
+	public void emptyMuseumTest() {
+		Museum empty = new Museum("emptyMuseum", null);
+		empty.getAverageRate();
+	}
+	
+	//un metodo che verifica che è possibile istanziare un 
+	//RandomMuseum con n pari a 1000 entro 2 secondi
+	 @Test(timeout = 2000)
+	 public void timeTest() {
+		 RandomMuseum random = new RandomMuseum(1000);
+	 }
+}
--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
@@ -1,5 +1,48 @@
 package it.unical.demacs.ingsw2019.PracticeTestB;
 
-public class MuseumsTest {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MuseumsTest {
+	
+	private static LouvreMuseum louvre;
+	private static BritishMuseum british;
+	
+	@BeforeClass
+	public static void initMuseums() {
+		louvre = new LouvreMuseum();
+		british = new BritishMuseum();
+	}
+
+	//un metodo che verifichi che il punteggio medio di una 
+	//istanza di LouvreMuseum sia superiore al punteggio 
+	//medio di una istanza di BritishMuseum
+	@Test
+	public void louvreBritishAverageRateTest() {
+		assertTrue(louvre.getAverageRate()>british.getAverageRate());
+	}
+	
+	/* un metodo che verifichi che il punteggio medio 
+	 * di una istanza di LouvreMuseum sia superiore al 
+	 * punteggio medio di una istanza di RandomMuseum con 100 opere casuali 
+	 */
+	@Test
+	public void louvreRandomAverageRateTest() {
+		RandomMuseum random = new RandomMuseum(100);
+		assertTrue(louvre.getAverageRate()>random.getAverageRate());
+	}
+	
+	/*un metodo che verifichi che la differenza in valore assoluto 
+	 * tra i punteggi medi di due istanze di RandomMuseum contenenti 
+	 * 1000 opere sia inferiore a 1.5
+	 */
+	@Test
+	public void absoluteValueTest() {
+		RandomMuseum r1 = new RandomMuseum(1000);
+		RandomMuseum r2 = new RandomMuseum(1000);
+		double diff = Math.abs((r1.getAverageRate()-r2.getAverageRate()));
+		assertTrue(diff<1.5);
+	}
 }
