6eb8711dc52a1de5a53305e2ce393ac6e108463e
alexf <alexf@160.97.140.225>
2019-10-31 16:48:37 +0100

    museum class name changed

--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTestSuite.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({MuseumsTest.class, MuseumsAdvancedTest.class})
+@SuiteClasses({MuseumsTest.class, MuseumsExtraTest.class})
 
 public class MuseumsTestSuite {
 
