8604335f4bf3c3e951f5ae7c73256477da262744
defeat <defeat@defeated>
2019-10-31 15:39:24 +0000

    MuseumsAdvancedTest to MuseumExtraTest

--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsExtraTest.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsExtraTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MuseumsAdvancedTest {
+public class MuseumsExtraTest {
 
 	/*un metodo che verifica che getAverageRate() 
 	 * sollevi un’eccezione su un museo senza opere
