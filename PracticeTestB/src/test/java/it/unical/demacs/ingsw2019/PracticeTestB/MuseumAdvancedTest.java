c102b27bb368947b05ff932fb9e9394f8fdf6ece
ralex <alessandrorocca_ar@hotmail.it>
2019-10-31 16:29:08 +0100

    added new tests

---
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumAdvancedTest.java
@@ -0,0 +1,20 @@
+package it.unical.demacs.ingsw2019.PracticeTestB;
+
+import org.junit.Test;
+
+public class MuseumAdvancedTest {
+
+	@Test(expected = RuntimeException.class)
+	public void averageExceptionWork()
+	{
+		RandomMuseum random = new RandomMuseum(0);
+		
+		random.getAverageRate();
+	}
+	
+	@Test(timeout = 2000)
+	public void averegeTimeWork()
+	{
+		RandomMuseum random = new RandomMuseum(1000);
+	}
+}
--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
@@ -1,5 +1,38 @@
 package it.unical.demacs.ingsw2019.PracticeTestB;
 
-public class MuseumsTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class MuseumsTest {
+	
+	@Test
+	public void averageLouvreBritishWork()
+	{
+		LouvreMuseum louvre = new LouvreMuseum();
+		BritishMuseum british = new BritishMuseum();
+		
+		assertEquals(true, louvre.getAverageRate() >  british.getAverageRate() );
+	}
+	
+	@Test
+	public void averageLouvreRandomWork()
+	{
+		LouvreMuseum louvre = new LouvreMuseum();
+		RandomMuseum random = new RandomMuseum(100);
+		
+		assertEquals(true, louvre.getAverageRate() >  random.getAverageRate() );
+	}
+	
+	@Test
+	public void differenceRandomWork()
+	{
+		RandomMuseum random = new RandomMuseum(1000);
+		RandomMuseum random2 = new RandomMuseum(1000);
+		
+		double difference = Math.abs(random.getAverageRate() - random2.getAverageRate());
+		
+		assertEquals(true, difference < 1.5);
+		
+	}
 }
