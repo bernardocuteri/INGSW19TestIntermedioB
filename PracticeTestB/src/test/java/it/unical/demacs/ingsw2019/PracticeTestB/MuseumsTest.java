9db539c48e4a3ec5d4efa6fd670c2641ef099745
luca laboccetta <luca@luca-ThinkPad-T460s>
2019-10-31 17:28:18 +0100

    Test Completati.

--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTest.java
@@ -1,5 +1,30 @@
 package it.unical.demacs.ingsw2019.PracticeTestB;
 
-public class MuseumsTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class MuseumsTest {
+	@Test
+	public void punteggioMedio() {
+		BritishMuseum b=new BritishMuseum();
+		LouvreMuseum l=new LouvreMuseum();
+		assertEquals(true,l.getAverageRate()>b.getAverageRate());
+		
+		
+	}
+	@Test
+	public void punteggioMedioRandom() {
+		LouvreMuseum l=new LouvreMuseum();
+		RandomMuseum r=new RandomMuseum(100);
+		assertEquals(true,l.getAverageRate()>r.getAverageRate());
+	}
+	@Test
+	public void differenzaAssoluta() {
+		RandomMuseum r=new RandomMuseum(1000);
+		RandomMuseum ran=new RandomMuseum(1000);
+		assertEquals(true,(r.getAverageRate()-ran.getAverageRate())<1.5);
+		
+	}
+	
 }
