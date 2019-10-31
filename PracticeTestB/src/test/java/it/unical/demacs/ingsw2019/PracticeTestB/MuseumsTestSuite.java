d26b90942f420e90e6e5396c5eb12390eadaa75e
Leonardo <Leonardo@160.97.143.222>
2019-10-31 16:47:43 +0100

    Aggiunta dei Metodi Richiesti Dal Prof

--- PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTestSuite.java
+++ PracticeTestB/src/test/java/it/unical/demacs/ingsw2019/PracticeTestB/MuseumsTestSuite.java
@@ -7,6 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({MuseumsTest.class,MuseumsAdvancedTest.class})
-public class MuseumsTestSuite {
+public class MuseumsTestSuite { 
 
 }
